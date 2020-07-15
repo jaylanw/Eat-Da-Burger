5587818fd2086e9c7dcef66fdabb56af346ec55a
Jaylan <jaylanwienckowski@gmail.com>
2020-07-15 12:39:44 -0500

    initial setup

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Cheddar Cheezy', '0');
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Baconator Burger', '0');
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ('Avocado Burger', '0');
+
