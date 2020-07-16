34cf1789ae9dcbee739889a4bab13ca592c60b4f
Jaylan <jaylanwienckowski@gmail.com>
2020-07-16 10:00:29 -0500

    setting up variables and routed in server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burgers_controller.js");
+var PORT = process.env.PORT || 3306;
+
+var app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use("/", routes);
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
